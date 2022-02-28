6b71a5de246db08498afb022a7c239949b904085
xcodecat <xmissh0lyc4t@gmail.com>
2022-02-28 10:37:15 +0100

    changed prefix

--- extensions/utility.py
+++ extensions/utility.py
@@ -3,9 +3,9 @@ from datetime import datetime
 import hikari
 import lightbulb
 
-info_plugin = lightbulb.Plugin("Info")
+utility = lightbulb.Plugin("Utility")
 
-@info_plugin.command
+@utility.command
 @lightbulb.option("target", "The member to get information about.", hikari.User, required=False)
 @lightbulb.command("userinfo", "Get info on a server member.")
 @lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
@@ -21,35 +21,29 @@ async def userinfo(ctx: lightbulb.Context) -> None:
             title=f"User Info - {target.display_name}",
             description=f"ID: `{target.id}`",
             colour=0x3B9DFF,
-            timestamp=datetime.now().astimezone(),
-        )
+            timestamp=datetime.now().astimezone(),)
         .set_footer(
-            text=f"Requested by {ctx.member.display_name}",
+            text=f"Requested by {ctx.member.display_name} • {ctx.author.id}',",
             icon=ctx.member.avatar_url or ctx.member.default_avatar_url,
         )
         .set_thumbnail(target.avatar_url or target.default_avatar_url)
         .add_field(
             "Bot?",
             str(target.is_bot),
-            inline=True,
-        )
+            inline=True,)
         .add_field(
             "Created account on",
             f"<t:{created_at}:d>\n(<t:{created_at}:R>)",
-            inline=True,
-        )
+            inline=True,)
         .add_field(
             "Joined server on",
             f"<t:{joined_at}:d>\n(<t:{joined_at}:R>)",
-            inline=True,
-        )
+            inline=True,)
         .add_field(
             "Roles",
             ", ".join(r.mention for r in roles),
-            inline=False,
-        )
-    )
+            inline=False,))
     await ctx.respond(embed)
 
 def load(bot: lightbulb.BotApp) -> None:
-    bot.add_plugin(info_plugin)
+    bot.add_plugin(utility)
--- main.py
+++ main.py
@@ -8,21 +8,28 @@ dotenv.load_dotenv()
 
 bot = lightbulb.BotApp(
     os.environ["BOT_TOKEN"],
-    prefix="+",
+    prefix="h!",
     banner=None,
     intents=hikari.Intents.ALL,
     default_enabled_guilds=(845711749944705035))
 
 @bot.command
 @lightbulb.command("ping", description="The bot's ping")
-@lightbulb.implements(lightbulb.PrefixCommand)
+@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
 async def ping(ctx: lightbulb.Context) -> None:
     await ctx.respond(f"Pong! Latency: {bot.heartbeat_latency*1000:.2f}ms")
+
+@bot.command()
+@lightbulb.option("text", "Text to repeat")
+@lightbulb.command("echo", "Repeats the user's input")
+@lightbulb.implements(lightbulb.SlashCommand, lightbulb.PrefixCommand)
+async def echo(ctx: lightbulb.Context) -> None:
+    await ctx.respond(ctx.options.text)
 
 if __name__ == "__main__":
     if os.name != "nt":
         import uvloop
         uvloop.install()
 
-    bot.run()
     bot.load_extensions_from("./extensions/", must_exist=True)
+    bot.run()
